9fd8120fff25d6966f02803fb6c6bc95ea51531c
Adinath-S-Pawar <21aspawar16055@gmail.com>
2026-02-15 17:06:17 +0530

    Fix socket backend URL via Vercel env var

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-
+console.log("BACKEND URL:", import.meta.env.VITE_BACKEND_URL);
 export const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket"],
   autoConnect: true,
